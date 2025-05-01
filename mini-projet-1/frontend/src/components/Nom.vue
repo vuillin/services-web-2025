327bb3c5fa6126e478abcd3080dec4ffb1cf9c1b
VUILLIN Thomas <144356846+vuillin@users.noreply.github.com>
2025-05-01 15:44:35 +0200

    Add files via upload

---
+++ mini-projet-1/frontend/src/components/Nom.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="nom">
+      <p>Ibrahim Balta - Thomas Vuillin - Mini-projet : n°1</p>
+    </div>
+  </template>
+  
+  <style scoped>
+  .nom {
+    position: fixed;
+    top: 10px;
+    left: 50px;
+    color: #888;
+    font-size: 16px;
+    z-index: 1000;
+  }
+  </style>
+  
